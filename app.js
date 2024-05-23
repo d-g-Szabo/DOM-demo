61b71b53dd90b47bb0ffd663616c1de99b822b78
Daniel Szabo <d.g.szabo-2017@hull.ac.uk>
2024-05-23 11:35:05 +0100

    feat: Add new text and list elements dynamically
    This commit adds functionality to dynamically add new text and list elements to the HTML document. It appends a new text to the existing h2 element and replaces the entire text content of the h2 element. Additionally, it creates a new h1 heading and a list with three list items, appending them to the body of the document.

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ console.log(mainContainer);
 const h2 = document.querySelector("h2");
 console.log(h2);
 
+h2.append(" - this is a new text");
+h2.textContent = "This is replacing the a whole text";
+
 //selecting multiple elements --> querySelectorAll
 const allParagraphs = document.querySelectorAll("p");
 console.log(allParagraphs);
@@ -35,8 +38,25 @@ mainContainer.remove(newImage);
 const body = document.querySelector("*");
 body.style.backgroundColor = "lightblue";
 
+const h1 = document.createElement("h1");
+h1.textContent = "This is a Heading!";
+body.appendChild(h1);
+
+const list = document.createElement("ul");
+const listItem1 = document.createElement("li");
+listItem1.textContent = "This is fisrt";
+const listItem2 = document.createElement("li");
+listItem2.textContent = "This is second";
+const listItem3 = document.createElement("li");
+listItem3.textContent = "This is third";
+list.appendChild(listItem1);
+list.appendChild(listItem2);
+list.appendChild(listItem3);
+body.appendChild(list);
+
 //change font family
 const fontFamily = (document.body.style.fontFamily = "monospace");
 
+//deleting everything from js
 // let htmlDocument = document.documentElement;
 // htmlDocument.remove(document);
